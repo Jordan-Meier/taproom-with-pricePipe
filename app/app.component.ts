f118d4d89926cb5a422e80c2596f9190daf37b38
Brad Pizzimenti and Jordan Meier <jbirdmeier@gmail.com>
2016-03-23 09:57:29 -0700

    create a keg model and stock data for a single keg object in appComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from 'angular2/core';
+import { Keg } from './keg.model'
 
 @Component({
   selector: 'my-app',
   template: `
-    <h1>Skeleton Angular2 App!</h1>
+  <div class="jumbotron text-center">
+    <h1>Welcome to the Tap Room</h1>
+  </div>
+  <div class='container'>
+    <h2>{{kegs[0].brewery}}</h2>
+  </div>
   `
 })
 
 export class AppComponent {
-
+  public kegs: Keg[];
+  constructor() {
+    this.kegs = [
+      new Keg("Deschuttes", "Angular Stout", 0, "7%"),
+    ];
+    console.log(this.kegs[0]);
+  }
 }
---
+++ app/keg.model.ts
@@ -0,0 +1,6 @@
+export class Keg {
+  public pintsRemaining: number = 124;
+  public pintPrice: number = 5;
+
+  constructor(public brewery: string, public brewName: string, public id: number, public alcPercent: string){}
+}
